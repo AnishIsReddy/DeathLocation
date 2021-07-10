d7f0dc0fc83e429f816af38c1aba3731bd4cf0e8
Anish <anishnreddy2095@gmail.com>
2021-07-10 13:15:36 -0400

    Initial commit

---
+++ src/main/java/us/reddyornot/App.java
@@ -0,0 +1,17 @@
+package us.reddyornot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("DeathLocation initialized");
+        getServer().getPluginManager().registerEvents(new ListenerClass(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        assert true;
+    }
+}
+
---
+++ src/main/java/us/reddyornot/ListenerClass.java
@@ -0,0 +1,32 @@
+package us.reddyornot;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+
+
+public class ListenerClass implements Listener{
+    
+    @EventHandler
+    public void sendDeathCoords(PlayerDeathEvent event){
+        Player reciever = event.getEntity();
+        Location pos = reciever.getLocation();
+        
+        String WorldName = "";
+        if (pos.getWorld().getName().equals("world")){
+            WorldName = "Overworld";
+
+        }else if (pos.getWorld().getName().equals("world_nether")){
+            WorldName = "Nether";
+
+        }else if (pos.getWorld().getName().equals("world_the_end")){
+            WorldName = "End";
+        }
+
+        
+        reciever.sendMessage(ChatColor.RED + "You died at [" + pos.getBlockX() + ", " + pos.getBlockY() + ", " + pos.getBlockZ() + "] in the " + WorldName);
+    }
+}
